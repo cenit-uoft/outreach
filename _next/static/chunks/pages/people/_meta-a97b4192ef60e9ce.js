5485b69214ac70e6c18138d84666639060781355
Morteza <moghaddasianm@gmail.com>
2026-07-14 19:53:25 -0400

    Deploy flat HTML compilation update

---
+++ _next/static/chunks/pages/people/_meta-a97b4192ef60e9ce.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[561],{4038:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/people/_meta",function(){return _(8180)}])},8180:function(n,u,_){"use strict";_.r(u),_.d(u,{__N_SSG:function(){return t}});var t=!0;u.default=()=>null}},function(n){n.O(0,[888,774,179],function(){return n(n.s=4038)}),_N_E=n.O()}]);
