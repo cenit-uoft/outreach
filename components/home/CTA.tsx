fd1ffd8eb24e09ab163c4ad83784489b1c493970
Morteza <moghaddasianm@gmail.com>
2026-07-16 15:39:06 -0400

    reduce the font size

--- components/home/CTA.tsx
+++ components/home/CTA.tsx
@@ -3,8 +3,8 @@ export default () => {
         <section style={{background:"#000099"}} className=" py-8">
             <div className="max-w-screen-xl mx-auto px-4 gap-x-12 justify-center items-center md:flex md:px-8">
                 <div className="max-w-4xl">
-                    <p className="mt-2 text-white text-3xl font-bold">
-                        We are reimagining how far more processing power, storage capacity, and machine intelligence inside the network devices and the Internet at large to attain intelligent digital infrastructures that are efficient, secure, resilient, and available to all segments of society. 
+                    <p className="mt-2 text-white text-xl font-bold">
+                        We are reimagining communication protocols and network architectures to attain intelligent digital infrastructures that are efficient, secure, resilient, and available to all segments of society.
                     </p>
                 </div>
             </div>
